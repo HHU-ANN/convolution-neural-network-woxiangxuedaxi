1c7c55256fdee541136c251577491bc2a12a8765
DING <98734433+woxiangxuedaxi@users.noreply.github.com>
2023-06-30 11:58:40 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -8,18 +8,15 @@ os.system("sudo pip3 install torch")
 os.system("sudo pip3 install torchvision")
 
 
-
-
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-num_epochs = 25  # 50轮
-batch_size = 50  # 50步长
-learning_rate = 0.011  # 学习率0.01
+num_epochs = 25  
+batch_size = 50  
+learning_rate = 0.012
 from torch.utils.data import DataLoader
 #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 device = torch.device('cpu')
@@ -38,12 +35,12 @@ def read_data():
     data_loader_val = DataLoader(dataset=dataset_val, batch_size=batch_size, shuffle=False)
     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
-# 3x3 卷积定义
+
 def conv3x3(in_channels, out_channels, stride=1):
     return nn.Conv2d(in_channels, out_channels, kernel_size=3,
                      stride=stride, padding=1, bias=False)
 
-   # Resnet 的残差块
+   
 class ResidualBlock(nn.Module):
         def __init__(self, in_channels, out_channels, stride=1, downsample=None):
             super(ResidualBlock, self).__init__()
@@ -68,7 +65,7 @@ class ResidualBlock(nn.Module):
             return out
 
 class NeuralNetwork(nn.Module):
-    # ResNet定义
+
     def __init__(self, block, layers, num_classes=10):
         super(NeuralNetwork, self).__init__()
         self.in_channels = 16
@@ -108,56 +105,9 @@ class NeuralNetwork(nn.Module):
 
 model = NeuralNetwork(ResidualBlock, [2, 2, 2]).to(device)
 
-# # 损失函数
-# criterion = nn.CrossEntropyLoss()
-# optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-# 
-# # 更新学习率
-# def update_lr(optimizer, lr):
-#         for param_group in optimizer.param_groups:
-#             param_group['lr'] = lr
-# 
-# train_dataset, test_dataset, train_loader, test_loader = read_data()
-# # 训练数据集
-# total_step = len(train_loader)
-# curr_lr = learning_rate
-# for epoch in range(num_epochs):
-#     for i, (images, labels) in enumerate(train_loader):
-#         images = images.to(device)
-#         labels = labels.to(device)
-# 
-#         # Forward pass
-#         outputs = model(images)
-#         loss = criterion(outputs, labels)
-# 
-#         # Backward and optimize
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-# 
-#         if (i + 1) % 100 == 0:
-#             print("Epoch [{}/{}], Step [{}/{}] Loss: {:.4f}"
-#                     .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
-# 
-#         # 延迟学习率
-#     if (epoch + 1) % 20 == 0:
-#         curr_lr /= 3
-#         update_lr(optimizer, curr_lr)
-# 
-# torch.save(model.state_dict(), 'model.pth')
-
-# 
-# def read_data():
-#     # 这里可自行修改数据预处理，batch大小也可自行调整
-#     # 保持本地训练的数据读取和这里一致
-#     dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
-#     dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
-#     data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
-#     data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
-#     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
 def main():
-    model = NeuralNetwork(ResidualBlock, [2, 2, 2]).to(device) # 若有参数则传入参数
+    model = NeuralNetwork(ResidualBlock, [2, 2, 2]).to(device) 
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
     model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
